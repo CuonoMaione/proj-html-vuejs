34146676718db9810988c28c657482ecbb239a88
CuonoMaione <cuonomaione99@outlook.it>
2023-06-27 10:06:23 +0200

    vue init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template >
+  
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+
+</template>
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+<style lang="scss">
+@import '../src/styles/general.scss';
+@import 'bootstrap/scss/bootstrap';
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template >
+    <main>
+        
+    </main>
+</template>
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+<style lang="scss">
+    
+</style>
